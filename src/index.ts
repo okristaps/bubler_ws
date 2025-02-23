78ea3f760111b4c0a7a06113b77a141c40b49bbe
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-23 19:56:18 +0200

    allowed origins

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { handleWebSocket } from './game-socket';
 
+const ALLOWED_ORIGINS = [
+	'https://bubler.club',
+	'https://bubbler-env-game-chris-projects-e9171c8b.vercel.app',
+	'https://test.game.bubler.club',
+];
+
 export interface Env {
 	IC_HOST: string;
 	IC_CANISTER_ID: string;
@@ -9,9 +15,15 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
+		const origin = request.headers.get('Origin') || '';
+		if (env.NODE_ENV !== 'development' && !ALLOWED_ORIGINS.includes(origin)) {
+			return new Response('Forbidden', { status: 403 });
+		}
+
 		if (request.headers.get('Upgrade') === 'websocket') {
 			return handleWebSocket(request, env);
 		}
+
 		return new Response(JSON.stringify({ message: 'API Running' }), {
 			headers: { 'Content-Type': 'application/json' },
 			status: 200,
